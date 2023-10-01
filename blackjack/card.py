7a2eda71340ffe5195656b7784fd8e35595fd64f
HJoonKwon <dsazz801@gmail.com>
2023-10-01 15:37:27 +0900

    update: card class

--- blackjack/card.py
+++ blackjack/card.py
@@ -4,12 +4,15 @@ class Suit(Enum):
     SPADES, HEARTS, DIAMONDS, CLUBS = 'spades', 'hearts', 'diamonds', 'clubs'
 
 class Card:
-    def __init__(self):
-        pass 
+    def __init__(self, value: int, suit: Suit):
+        assert 1 <= value <= 10, f'value = {value} should be >=1 and <=10'
+        assert isinstance(suit, Suit), 'invalid suit'
+        
+        self._value = value 
+        self._suit = suit 
     
-    def get_value(self):
-        pass 
+    def get_value(self) -> int:
+        return self._value 
     
-    def get_suit(self):
-        # spades/ hearts / diamonds / clubs 
-        pass 
+    def get_suit(self) -> Suit:
+        return self._suit 
