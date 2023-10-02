2905d16e9ef3d22e74991d8b69300a12d878e70d
HJoonKwon <dsazz801@gmail.com>
2023-10-02 18:34:47 +0900

    update: implement a simple app script

--- blackjack/app.py
+++ blackjack/app.py
@@ -1,8 +1,4 @@
 from game import Game 
-from deck import Deck 
-from player import UserPlayer, Dealer 
 
-deck = Deck() 
-user = UserPlayer() 
-dealer = Dealer()
-game = Game()
+game = Game() 
+game.play_round() 
