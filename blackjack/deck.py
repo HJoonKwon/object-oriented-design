42fea65cf3b44cce0f2ae50282b103de8bdd9592
HJoonKwon <dsazz801@gmail.com>
2023-10-02 13:30:02 +0900

    update: implement deck calss

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -1,13 +1,21 @@
+import random 
+from card import Card, Suit 
 
 class Deck:
     def __init__(self):
-        pass 
-    
+        self._cards = [] 
+        for suit in Suit:
+            for i in range(1, 14):
+                card = Card(i, suit) if i < 11 else Card(10, suit)
+                self._cards.append(card)
+        self.shuffle()
+        
     def draw(self):
-        # draw a card in order 
-        pass 
+        return self._cards.pop(0) 
     
     def shuffle(self):
-        pass 
+        for i in range(len(self._cards)):
+            j = random.randrange(0, len(self._cards)-1)
+            self._cards[i], self._cards[j] = self._cards[j], self._cards[i]
     
     
