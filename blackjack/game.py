a891b9e044bed11e79d202cb7ca3d65bd21af34a
HJoonKwon <dsazz801@gmail.com>
2023-10-02 18:20:03 +0900

    update: implement game round

--- blackjack/game.py
+++ blackjack/game.py
@@ -1,38 +1,78 @@
+from deck import Deck 
+from player import UserPlayer, Dealer, Hand 
 
 class Game:
     
     def __init__(self):
-        # init deck 
-        # init players
-        # draw two cards for user player/dealer
-        # one card of dealer is hidden
-        
-        # print game status 
-        pass 
+        self._deck = Deck() 
+        user_money = int(input(f"Type in how much money you want to deposit: "))
+        self._user = UserPlayer(Hand(), user_money)
+        self._dealer = Dealer(Hand())
     
     def play_round(self):
         
-        # user make move 
-        # while user input --> draw yes/no 
-        # if score >21 -> dealear wins 
-        # else pass 
+        for _ in range(2):
+            card = self._deck.draw()
+            self._user.add_card(card)
+            
+            card = self._deck.draw() 
+            self._dealer.add_card(card)
         
-        # dealer make move 
-        # draw while score < target_score
-        # if score > 21 --> user wins 
-        # else pass 
+        print(f"user: {self._user.get_hand().get_cards()} \n")
+        print(f"dealer: {self._dealer.get_hand().get_cards()[0]}, X \n")
         
-        # check winner 
-        # compare two scores 
+        winning_money = 0 
         
-        # print game status 
-        pass 
-    
-    def print_status(self):
-        pass
+        while self._user.make_move():
+            print("User Draw: \n")
+            bet_money = int(input(f"Bet money. {1}-{self._user.get_money()}: "))
+            self._user.bet_money(bet_money)
+            winning_money += bet_money
+            card = self._deck.draw() 
+            self._user.add_card(card)
+            if self._user.get_hand().get_score() > 21: 
+                print(f"User score is over 21. You lose! Your current balance is {self._user.get_money()}")
+                self.reset_game()
+                return 
+            else:
+                print(f"User score is {self._user.get_hand().get_score()}")
+        
+        while self._dealer.make_move():
+            print("Dealer Draw: \n")
+            card = self._deck.draw() 
+            self._dealer.add_card(card)
+            if self._dealer.get_hand().get_score() > 21:
+                self._user.add_money(winning_money * 2)
+                print(f"Dealer score is over 21. You win! Your current balance is {self._user.get_money()}")
+                self.reset_game()
+                return 
+            else:
+                print(f"Dealer score is {self._dealer.get_hand().get_score()}")
+        
+        user_score = self._user.get_hand().get_score() 
+        dealer_score = self._dealer.get_hand().get_score() 
+        print(f"user score: {user_score}, dealer_score: {dealer_score} \n")
+        if user_score > dealer_score:
+            self._user.add_money(winning_money * 2)
+            print(f"You win! Your current balance is {self._user.get_money()}")
+        elif user_score < dealer_score:
+            print(f"You lose! Your current balance is {self._user.get_money()}")
+        else:
+            self._user.add_money(winning_money)
+            print(f"You draw! Your current balance is {self._user.get_money()}")
+        self.reset_game()
+        return 
     
     def reset_game(self):
+        if self._user.get_money() < 1:
+            print(f"Your money is ran out. The game is over.")
+            return 
+        if input("Start new round? y/n: ") != "y":
+            print("Finish the game!")
+            return 
+            
+        self._deck = Deck()
+        self._user.clear_hand()
+        self._dealer.clear_hand()
+        self.play_round()  
         
-        # reset players and deck 
-        
-        pass 
